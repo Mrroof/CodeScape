acdb5020ae84bc467146467df6414009b02db8a3
MR.FITZ <rulej31@gmail.com>
2023-08-10 22:01:37 -0700

    update

--- joelproject/weatherapp/views.py
+++ joelproject/weatherapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+import requests
+from django.conf import settings 
+from django.http import JsonResponse
 # Create your views here.
+def get_weather(request, location):
+    api_key = settings.WEATHER_API_KEY
+    url = f'https://api.weatherapi.com/v1/current.json?key={"2c5fb582e63f4767973123737230908"}&q={location}'
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data)
