e399717b2a7d18acc99afa0605dae5db4767c45f
MR.FITZ <rulej31@gmail.com>
2023-08-24 15:08:33 -0700

    update

--- joelproject/weatherapp/views.py
+++ joelproject/weatherapp/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 def get_weather(request):
     city = request.GET.get('city')
     api_key = settings.WEATHER_API_KEY
-    url = f'https://api.weatherapi.com/v1/current.json?key={"2c5fb582e63f4767973123737230908"}&q={city}'
+    url = f'https://api.weatherapi.com/v1/current.json?key=2c5fb582e63f4767973123737230908&q=London&aqi=no'
     
     
     response = requests.get(url)
