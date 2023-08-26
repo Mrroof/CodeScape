398382ad3ebafb94ecc4c59680676e3c32c31e9f
MR.FITZ <rulej31@gmail.com>
2023-08-26 00:18:27 -0700

    update

--- joelproject/weather/views.py
+++ joelproject/weather/views.py
@@ -1,5 +1,10 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    url= 'http://api.weatherapi.com/v1/current.json?key=ef910f5466bc4a1d83e00203232608&q=London&aqi=no'
+    city= 'London'
+
+    r = requests.get(url.format(city))
     return render(request, 'weather/weather.html')
