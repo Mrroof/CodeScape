971c93d4f00659d09e3032375afebe766e6dc54e
MR.FITZ <rulej31@gmail.com>
2023-08-26 22:34:25 -0700

    update

--- compsciproj/taskmanager/views.py
+++ compsciproj/taskmanager/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def task_list(request):
+    incomplete_tasks = ["Task 1", "Task 2"]  # Replace with your data
+    completed_tasks = ["Task 3", "Task 4"]    # Replace with your data
+    return render(request, 'tasks.html', {'incomplete_tasks': incomplete_tasks, 'completed_tasks': completed_tasks})
