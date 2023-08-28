6cdefaa6f511af2f17bca35ab3c2ec8f58841b3b
MR.FITZ <rulej31@gmail.com>
2023-08-28 03:27:46 -0700

    bg

--- login/user/forms.py
+++ login/user/forms.py
@@ -12,3 +12,18 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'phone_no', 'password1', 'password2']
+
+
+from django.shortcuts import render
+from .forms import UserRegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Handle successful registration (e.g., redirect)
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'user/registration.html', {'form': form, 'title': 'register here'})
