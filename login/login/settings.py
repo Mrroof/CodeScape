96ac24776d09724844111ffef074719c368964a6
MR.FITZ <rulej31@gmail.com>
2023-08-28 02:36:44 -0700

    hgfygu

--- login/login/settings.py
+++ login/login/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'user.apps.UserConfig',
     'crispy_forms',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
