a977bd9be01ccc5d2f23c405ef76396869acddc2
MR.FITZ <rulej31@gmail.com>
2023-08-28 01:50:46 -0700

    gjjyu

--- login/login/settings.py
+++ login/login/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user.apps.UserConfig',
-    'crispy_forms',
+    'crispy_form',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
