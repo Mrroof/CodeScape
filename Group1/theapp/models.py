2eb0b963d306dc4a40a8cee035edf52948947e68
MR.FITZ <rulej31@gmail.com>
2023-08-30 02:32:45 -0700

    yes

--- Group1/theapp/models.py
+++ Group1/theapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Task(models.Model):
+    user =models.ForeignKey(User, on_deLete=models.CASCADE,null =True, bLank=True)
+    title = models.CharField(max_Length=200)
+    description =models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    create = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['complete']
+
