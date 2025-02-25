4d935454b10c7b91bd6e6550a3d624c6e531b1ea
MR.FITZ <rulej31@gmail.com>
2025-02-25 08:05:38 +0000

    update

--- Everydaylifestyle/fare_per_age.py
+++ Everydaylifestyle/fare_per_age.py
@@ -9,5 +9,6 @@ titanic = sns.load_dataset('titanic')
 # TODO: divide the fare by the age to create a feature 'fare_per_age'. Use DataFrame operations. Any non-number should be replaced with 0.
 titanic['age'] = titanic['age'].replace(np.nan, 0)
 titanic['fare_per_age'] = titanic['fare'] / titanic['age']
+titanic['fare_per_age'].replace([np.inf, -np.inf], 0, inplace=True)
 # Print the first 10 rows of the titanic dataframe
 print(titanic.head(10))
